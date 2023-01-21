964e164d5f97659f4a4a8ba712cc3549b658d109
Raynara Santiago <raysantiagobz@gmail.com>
2023-01-21 20:28:44 -0300

    Fluxo comum-page login 01 e 02

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/login" component={ Login } />
+    </Switch>
   );
 }
-
 export default App;
