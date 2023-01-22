ffe289fb1293bfc8b4d1e742d937f0e48c95c31a
Raynara Santiago <raysantiagobz@gmail.com>
2023-01-22 18:54:39 -0300

    corrigindo erro de lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,13 +1,12 @@
-const controllerLogin = require('./controller/login')
-const cors = require('cors');
 const express = require('express');
+const controllerLogin = require('./controller/login');
 
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.post('/login', controllerLogin.login);
-
 
 module.exports = app;
