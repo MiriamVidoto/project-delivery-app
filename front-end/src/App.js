a56727276c6b963f5ae412bc5aa630f6575b7e24
Raynara Santiago <raysantiagobz@gmail.com>
2023-01-23 10:55:17 -0300

    comum-login register req 06, 07 e 08

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
     </Switch>
   );
 }
---
+++ front-end/src/pages/Register.js
@@ -0,0 +1,54 @@
+import { validate } from 'email-validator';
+import React, { useState } from 'react';
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const register = () => {
+    const numberTwelve = 12;
+    const numberSix = 6;
+    return (validate(email)
+      && password.length >= numberSix
+      && name.length >= numberTwelve);
+  };
+  return (
+    <div>
+      <label htmlFor="email">
+        Nome
+        <input
+          data-testid="common_register__input-name"
+          name="name"
+          type="name"
+          onChange={ (e) => setName(e.target.value) }
+        />
+      </label>
+      <label htmlFor="email">
+        Email
+        <input
+          data-testid="common_register__input-email"
+          name="email"
+          type="email"
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          data-testid="common_register__input-password"
+          name="password"
+          type="password"
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+      <button
+        data-testid="common_register__button-register"
+        type="button"
+        disabled={ !register() }
+      >
+        Cadastrar
+      </button>
+    </div>
+  );
+}
