e8343c917c1881489dc06c69643e762cac85bba2
Raynara Santiago <raysantiagobz@gmail.com>
2023-01-23 16:22:55 -0300

    comum-login register req 06, 07, 08 e inicio do 09 comentado

---
+++ back-end/src/api/controller/register.js
@@ -0,0 +1,10 @@
+// const service = require('../service/register');
+
+// const register = async (req, res) => {
+//   const result = await service.register(req.body);
+//   return res.status(result.status).json(result.message);
+// };
+
+// module.exports = {
+//   register,
+// };
---
+++ back-end/src/api/service/register.js
@@ -0,0 +1,16 @@
+// const { User } = require('../../database/models');
+
+// const register = async (body) => {
+//   const { email, password, name } = body;
+//   const userCreate = await User.create({
+//     email, password, name
+//   });
+//   if (userCreate) {
+//     return { status: 201, message: userCreate };
+//   }
+//   return { status: 400, message: 'user already exists'}
+// };
+
+// module.exports = {
+//   register,
+// };
