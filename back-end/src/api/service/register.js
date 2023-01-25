1818878f16fcb83f38e63958735f9b569dc08dea
erikarg <erikatrue@gmail.com>
2023-01-25 18:40:56 -0300

    feat: adiciona requisito 11

--- back-end/src/api/service/register.js
+++ back-end/src/api/service/register.js
@@ -1,11 +1,17 @@
 const { User } = require('../../database/models');
 
 const register = async (body) => {
+  const { name, email } = body;
+  const user = await User.findOne({
+    where: { name, email },
+    attributes: ['name', 'email'],
+    raw: true,
+  });
+  if (!user) {
   const userCreate = await User.create(body);
-  if (userCreate) {
-    return { status: 201, message: userCreate };
+  return { status: 201, message: userCreate };
   }
-  return { status: 400, message: 'user already exists' };
+  return { status: 409, message: 'Conflict' };
 };
 
 module.exports = {
