6163da5d0842fb6af9d5745edf330e5f4b22f1e9
gabrieladnr@gmail.com <gabrieladnr@gmail.com>
2023-01-26 13:51:32 -0300

    fix front-end lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const controllerLogin = require('./controller/login');
-const controllerRegister = require('./controller/register');
+const controllerCostumerProducts = require('./controller/costumerProducts');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get('/coffee', (_req, res) => res.status(418).end());
-app.post('/login', controllerLogin.login);
+app.get("/coffee", (_req, res) => res.status(418)
+    .end());
+app.post('/login', controllerLogin.login)
 app.post('/register', controllerRegister.register);
+app.post('/customer/products', controllerCostumerProducts.products);
 
 module.exports = app;
