9dbe40e1866052d37544192865140a3df852b3c5
gabrieladnr@gmail.com <gabrieladnr@gmail.com>
2023-01-27 09:05:48 -0300

    add images middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/images', express.static('public')); 
 app.get("/coffee", (_req, res) => res.status(418).end());
 app.post('/login', controllerLogin.login)
 app.post('/register', controllerRegister.register);
