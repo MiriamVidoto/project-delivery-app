9ec7ebe3509e2ab5a6dbdbedc73a49733380b2a2
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-01-26 22:06:57 -0300

    correção lint

--- back-end/src/api/authentication/JWT.ts
+++ back-end/src/api/authentication/JWT.ts
@@ -1,22 +1,22 @@
-import { sign, verify } from 'jsonwebtoken';
+// import { sign, verify } from 'jsonwebtoken';
 
-const secret = process.env.JWT_SECRET || 'seusecretdetoken';
+// const secret = process.env.JWT_SECRET || 'seusecretdetoken';
 
-const createToken = (data): string => {
-  const token = sign(data, secret, { expiresIn: '7d' });
-  return token;
-};
+// const createToken = (data): string => {
+//   const token = sign(data, secret, { expiresIn: '7d' });
+//   return token;
+// };
 
-const verifyToken = (token: string) => {
-  try {
-    const { id } = verify(token, secret);
-    return id;
-  } catch {
-    return false;
-  }
-};
+// const verifyToken = (token: string) => {
+//   try {
+//     const { id } = verify(token, secret);
+//     return id;
+//   } catch {
+//     return false;
+//   }
+// };
 
-export {
-  createToken,
-  verifyToken,
-};
+// export {
+  // createToken,
+//   verifyToken,
+// };
