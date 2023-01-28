cbbd34c985c0f26e2ba900559451734fb075b203
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2023-01-27 21:42:02 -0300

    adiciona estrutura das paginas CustomerOrders e CustomerOrderDetails

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import NavBar from '../components/navbar';
 import ProductCard from '../components/productCard';
+import { getDataFromLocalStorage } from '../utils/localStorage';
 
 function Products() {
   const user = getDataFromLocalStorage('user');
